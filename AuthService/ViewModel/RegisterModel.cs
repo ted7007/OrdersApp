ac72c91dc25b42cc9eb9207f07a53040f939127e
Александр Косилов <ted7007@yandex.ru>
2022-11-19 12:52:40 +1000

    fix: fix db and register model

--- AuthService/ViewModel/RegisterModel.cs
+++ AuthService/ViewModel/RegisterModel.cs
@@ -2,9 +2,9 @@
 
 public class RegisterModel
 {
-    public string? UserName { get; set; }
+    public string UserName { get; set; }
 
-    public string? Password { get; set; }
+    public string Password { get; set; }
 
-    public string? ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; set; }
 }
