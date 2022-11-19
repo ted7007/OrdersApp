dee65436d05bcdafb820ef9fdbd00e2e4e4734d8
Александр Косилов <ted7007@yandex.ru>
2022-11-19 12:52:14 +1000

    feat: try to realize auth in api

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,9 +1,26 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelotConfig.json");
 builder.Services.AddOcelot();
+
+var identityUrl = builder.Configuration.GetValue<string>("IdentityUrl");
+var authenticationProviderKey = "IdentityApiKey";
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(authenticationProviderKey, x =>
+    {
+        x.Authority = identityUrl;
+        x.RequireHttpsMetadata = false;
+        x.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidAudiences = new[] { "orders", "dishes" }
+        };
+    });
+
 
 var app = builder.Build();
 
