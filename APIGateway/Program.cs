ca86c6643ccd05c80ee179ec0b54b53ee74392a3
Александр Косилов <ted7007@yandex.ru>
2022-12-01 03:06:01 +1000

    feat: change gateway to yarp

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,30 +1,39 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("ocelotConfig.json");
-builder.Services.AddOcelot();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));;
 
 var identityUrl = builder.Configuration.GetValue<string>("IdentityUrl");
 var authenticationProviderKey = "IdentityApiKey";
 
-builder.Services.AddAuthentication()
-    .AddJwtBearer(authenticationProviderKey, x =>
-    {
-        x.Authority = identityUrl;
-        x.RequireHttpsMetadata = false;
-        x.TokenValidationParameters = new TokenValidationParameters()
-        {
-            ValidAudiences = new[] { "orders", "dishes" }
-        };
-    });
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization(options = >);
 
+// builder.Services.AddAuthentication()
+//     .AddJwtBearer(authenticationProviderKey, x =>
+//     {
+//         x.Authority = identityUrl;
+//         x.RequireHttpsMetadata = false;
+//         x.TokenValidationParameters = new TokenValidationParameters()
+//         {
+//             ValidAudiences = new[] { "orders", "dishes" }
+//         };
+//     });
+// builder.Services.AddAuthorization(options =>
+// {
+//     options.AddPolicy("InternalPolicy", policy =>
+//         policy.RequireAuthenticatedUser()
+//             .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme));
+// });
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapReverseProxy();
 
-await app.UseOcelot();
 
 app.Run();
